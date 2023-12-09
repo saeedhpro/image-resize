945b1a115d52ccb1b5615113244af2024811f426
Saeed Heydari <saeedhpro@gmail.com>
2023-12-09 10:29:14 +0330

    Completed

---
+++ app/Console/Commands/ResizeImages.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
+class ResizeImages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'images:resize';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Resize images in a specific directory';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $directory = storage_path('app/images'); // مسیر دلخواه تصاویر
+
+        $files = File::files($directory);
+
+        $this->info('Resizing images...');
+        $manager = new ImageManager(
+            new Driver()
+        );
+        foreach ($files as $file) {
+            $image = $manager->read($file);
+
+            $image->resize(100, 100);
+
+            $newPath = $directory . '/resized/' . $file->getFilenameWithoutExtension().'_'.Carbon::now()->format('Y_m_d_H_i_s').'.'.$file->getExtension();
+
+            $encoded = $image->toJpeg();
+            $encoded->save($newPath);
+        }
+
+        $this->info('Images resized successfully.');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ResizeImages;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,6 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command(ResizeImages::class)->hourly();
         // $schedule->command('inspire')->hourly();
     }
 
